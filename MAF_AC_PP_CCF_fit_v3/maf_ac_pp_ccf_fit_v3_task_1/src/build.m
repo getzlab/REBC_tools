949473dd8ffce817b2ef33e13182057ac9a1417e
Chip Stewart <stewart@broadinstitute.org>
2020-03-30 14:51:56 -0400

    build

--- MAF_AC_PP_CCF_fit_v3/maf_ac_pp_ccf_fit_v3_task_1/src/build.m
+++ MAF_AC_PP_CCF_fit_v3/maf_ac_pp_ccf_fit_v3_task_1/src/build.m
@@ -1,4 +1,10 @@
 pwd
+rmpath	/home/unix/stewart/Projects/Cancer/tools/matlab
+rmpath	/home/unix/stewart/Projects/tools/matlab
+rmpath	/home/unix/stewart/Projects/matlab
+rmpath	/home/unix/stewart/CancerGenomeAnalysis/trunk/matlab/mike
+rmpath	/home/unix/stewart/CancerGenomeAnalysis/trunk/matlab/seq
+rmpath	/home/unix/stewart/CancerGenomeAnalysis/trunk/matlab
 addpath ../../../AllelicCapSeg_PP_CCF_fit_v3/alleliccapseg_pp_ccf_fit_v3_task_1/src
 mcc -g -m fc_MAF_AC_PP_CCF_fit_v3;
 quit;
