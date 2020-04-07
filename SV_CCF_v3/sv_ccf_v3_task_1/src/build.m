6f5894b6d767337c49c427a5e6463ab68736edd4
Chip Stewart <stewart@broadinsitute.org>
2020-04-07 03:59:07 +0000

    build

---
+++ SV_CCF_v3/sv_ccf_v3_task_1/src/build.m
@@ -0,0 +1,9 @@
+pwd
+rmpath	/home/unix/stewart/Projects/Cancer/tools/matlab
+rmpath	/home/unix/stewart/Projects/tools/matlab
+rmpath	/home/unix/stewart/Projects/matlab
+rmpath	/home/unix/stewart/CancerGenomeAnalysis/trunk/matlab/mike
+rmpath	/home/unix/stewart/CancerGenomeAnalysis/trunk/matlab/seq
+rmpath	/home/unix/stewart/CancerGenomeAnalysis/trunk/matlab
+mcc -g -m fc_SV_CCF_v3;
+quit;
